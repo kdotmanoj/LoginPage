9951703e362e701ff0f080e6990f2cffad24288d
kdotmanoj <hsuper681@gmail.com>
2024-07-10 13:13:48 +0530

    first commit

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -20,12 +20,7 @@ const userSchema = new mongoose.Schema({
         required: true,
         default: 'user'
     },
-    otp: {
-        type: String
-    },
-    otpExpiry: {
-        type: Date
-    },
+    
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,11 +5,54 @@ const router = express.Router();
 
 router.post("/register", async (req, res) => {
 
+    try {
+
+        const userExists = await User.findOne({ email: req.body.email });
+
+        if (userExists) {
+            res.send({
+                "success": false,
+                "message": "User already exists"
+            })
+        }
+
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        req.body.password = hashedPassword;
+
+
+        const newUser = new User(req.body);
+        await newUser.save();
+        return res.json("User created")
+
+    } catch (error) {
+        res.json(error);
+    }
 
 });
 
 router.post("/login", async (req, res) => {
-  
+    if(!user){
+        res.send({
+            success : false,
+            message : "User does not exist, please register"
+        })
+    }
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+    if(!validPassword){
+        res.send({
+            success : false,
+            message : "Wrong Password"
+        })
+    }
+
+    res.send({
+        success : true,
+        message : "User Logged in"
+    })
 });
 
 
